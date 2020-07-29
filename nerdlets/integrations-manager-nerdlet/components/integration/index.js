ba0dd93a332e9e41e6142b45b8f3347e5ff3a876
Kav91 <kav.pather@gmail.com>
2020-07-29 17:59:15 +1000

    add disc configuration

--- nerdlets/integrations-manager-nerdlet/components/integration/index.js
+++ nerdlets/integrations-manager-nerdlet/components/integration/index.js
@@ -17,6 +17,8 @@ export default class IntegrationInfo extends React.PureComponent {
     this.state = {
       activeItem: 'Configuration',
       standardConfig: '',
+      discConfig: '',
+      k8sConfig: '',
       readme: ''
     };
   }
@@ -25,6 +27,7 @@ export default class IntegrationInfo extends React.PureComponent {
     const { selectedIntegration, pkgName, integrationType } = this.props;
     if (selectedIntegration) {
       let standardUrl = selectedIntegration.standard;
+      let discoveryUrl = selectedIntegration.discovery || null;
       const rawGithubUrl = 'https://raw.githubusercontent.com/newrelic/';
       const repo = selectedIntegration.git.replace(
         'https://github.com/newrelic/',
@@ -35,10 +38,21 @@ export default class IntegrationInfo extends React.PureComponent {
         standardUrl = `${rawGithubUrl}${pkgName}${standardUrl}`;
       }
 
+      if (discoveryUrl && !discoveryUrl.startsWith('http')) {
+        discoveryUrl = `${rawGithubUrl}${pkgName}${discoveryUrl}`;
+      }
+
       // get standard config
       fetch(standardUrl).then((response) =>
         response.text().then((data) => this.setState({ standardConfig: data }))
       );
+
+      if (discoveryUrl) {
+        // get discovery config
+        fetch(discoveryUrl).then((response) =>
+          response.text().then((data) => this.setState({ discConfig: data }))
+        );
+      }
 
       if (integrationType === 'product') {
         // get readme
@@ -52,7 +66,8 @@ export default class IntegrationInfo extends React.PureComponent {
   handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
   render() {
-    const { activeItem, standardConfig, readme, integrationType } = this.state;
+    const { activeItem, standardConfig, discConfig, readme } = this.state;
+    const { integrationType } = this.props;
 
     return (
       <DataConsumer>
@@ -65,6 +80,15 @@ export default class IntegrationInfo extends React.PureComponent {
                   active={activeItem === 'Configuration'}
                   onClick={this.handleItemClick}
                 />
+                {discConfig ? (
+                  <Menu.Item
+                    name="Discovery Configuration"
+                    active={activeItem === 'Discovery Configuration'}
+                    onClick={this.handleItemClick}
+                  />
+                ) : (
+                  ''
+                )}
                 {integrationType === 'product' ? (
                   <Menu.Item
                     name="README"
@@ -87,6 +111,25 @@ export default class IntegrationInfo extends React.PureComponent {
                   width="100%"
                   value={standardConfig}
                   onChange={(v) => this.setState({ standardConfig: v })}
+                  editorProps={{ $blockScrolling: true }}
+                />
+              </div>
+
+              <div
+                style={{
+                  display:
+                    activeItem === 'Discovery Configuration' && discConfig
+                      ? ''
+                      : 'none'
+                }}
+              >
+                <AceEditor
+                  mode="yaml"
+                  theme="monokai"
+                  name="configuration"
+                  width="100%"
+                  value={discConfig}
+                  onChange={(v) => this.setState({ discConfig: v })}
                   editorProps={{ $blockScrolling: true }}
                 />{' '}
               </div>
